d4291cbb8797574cb41d7aabf8950f45db430c79
Erika Cuby <kubickova.erika@gmail.com>
2025-04-16 22:22:52 +0200

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landing from "./pages/Landing";
 import Developer from "./pages/Developer";
 import Illustration from "./pages/Illustration";
 import usePreviousLocation from "./hooks/usePreviousLocation";
+import Footer from "./components/Footer";
 
 function App() {
   const location = useLocation();
@@ -14,20 +15,29 @@ function App() {
     (location.pathname === "/developer" ||
       location.pathname === "/illustration");
 
+  const isLandingPage = location.pathname === "/"; // Add this line
+
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Landing />} />
-        <Route
-          path="/developer"
-          element={<Developer transitionFromLanding={isLandingToSection} />}
-        />
-        <Route
-          path="/illustration"
-          element={<Illustration transitionFromLanding={isLandingToSection} />}
-        />
-      </Routes>
-    </AnimatePresence>
+    <>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/developer"
+            element={<Developer transitionFromLanding={isLandingToSection} />}
+          />
+          <Route
+            path="/illustration"
+            element={
+              <Illustration transitionFromLanding={isLandingToSection} />
+            }
+          />
+        </Routes>
+      </AnimatePresence>
+
+      {/* Show footer everywhere except the landing page */}
+      {!isLandingPage && <Footer />}
+    </>
   );
 }
 
